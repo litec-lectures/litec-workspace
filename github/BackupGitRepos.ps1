f8fc1ed4a8dfde557dfeb26025c95c11de50f623
Peter Haslinger <hasp@litec.ac.at>
2021-07-26 22:52:50 +0200

    added vscode workspace and backup of github repos

---
+++ github/BackupGitRepos.ps1
@@ -0,0 +1,98 @@
+Param (
+    #[String] $userName,
+    [String] $organisationName,
+    [String] $backupDirectory,
+
+    [ValidateRange(1,256)]
+    [Int] $maxConcurrency=8
+)
+
+Import-Module -Name PowerShellForGitHub
+
+# Default the backup directory to './YYYY-MM-DD'. This can
+# not be done in the Param section because $PSScriptRoot
+# will not be resolved if this script gets invoked from cmd.
+if (!$backupDirectory) {
+    $backupDirectory = $(Join-Path -Path "$PSScriptRoot" -ChildPath $(Get-Date -UFormat "%Y-%m-%d"))
+}
+
+# Measure the execution time of the backup script.
+$stopwatch = [System.Diagnostics.Stopwatch]::startNew()
+
+# Fetch all repos
+Write-Host "Fetching Repository List/Info from Github..."
+if ($organisationName) {
+    $repositories = Get-GitHubRepository -OrganizationName $organisationName 
+} else {
+    $repositories = Get-GitHubRepository -Affiliation owner
+}
+
+# Print a userfriendly message what will happen next.
+Write-Host "Cloning $($repositories.Count) repositories " -NoNewLine
+Write-Host "into '${backupDirectory}' with a maximum concurrency of ${maxConcurrency}:"
+
+# Clone each repository into the backup directory.
+ForEach ($repository in $repositories) {
+
+    while ($true) {
+
+        # Handle completed jobs as soon as possible.
+        $completedJobs = $(Get-Job -State Completed)
+        ForEach ($job in $completedJobs) {
+            $job | Receive-Job
+            $job | Remove-Job
+        }
+
+        $concurrencyLimitIsReached = $($(Get-Job -State Running).Count -ge $maxConcurrency)
+        if ($concurrencyLimitIsReached) {
+
+            $pollingFrequencyInMilliseconds = 50
+            Start-Sleep -Milliseconds $pollingFrequencyInMilliseconds
+            continue
+        }
+
+        # Clone or fetch a remote GitHub repository into a local directory.
+        $scriptBlock = {
+
+            Param (
+                [Parameter(Mandatory=$true)]
+                [String]
+                $fullName,
+
+                [Parameter(Mandatory=$true)]
+                [String]
+                $directory
+            )
+
+            if (Test-Path "${directory}") {
+
+                git --git-dir="${directory}" fetch --quiet --all
+                git --git-dir="${directory}" fetch --quiet --tags
+                Write-Host "[${fullName}] Backup completed with git fetch strategy."
+                return
+            }
+
+            git clone --quiet --mirror "https://github.com/${fullName}.git" "${directory}"
+            Write-Host "[${fullName}] Backup completed with git clone strategy."
+        }
+
+        # Suffix the repository directory with a ".git" to indicate a bare repository.
+        $directory = $(Join-Path -Path $backupDirectory -ChildPath "$($repository.name).git")
+
+        Write-Host "[$($repository.full_name)] Starting backup to ${directory}..." -ForegroundColor "DarkYellow"
+        Start-Job $scriptBlock -ArgumentList $repository.full_name, $directory | Out-Null
+
+        # Give the job some time to start.
+        $warmUpTimeoutInMilliseconds = 50
+        Start-Sleep -Milliseconds $warmUpTimeoutInMilliseconds
+
+        break
+    }
+}
+
+# Wait for the last jobs to complete and output their results.
+Get-Job | Receive-job -AutoRemoveJob -Wait
+
+$stopwatch.Stop()
+$durationInSeconds = [Math]::Floor([Decimal]($stopwatch.Elapsed.TotalSeconds))
+Write-Host "Successfully finished the backup in ${durationInSeconds} seconds." -ForegroundColor "Yellow"
---
+++ github/readme.md
@@ -0,0 +1,61 @@
+# GitHub Repository Tools and Infos
+
+## Used Links
+
+### Backup / Transfer
+
+- [Backup Gist](https://github.com/countzero/backup_github_repositories/blob/master/backup_github_repositories.ps1) - has a nice threading approach, uses REST API
+- [Local Backup](https://markb.uk/local-github-repository-backup-with-powershell.html) - simple, but uses GraphQL - nice!
+- [Difference --mirror / --bare](https://stackoverflow.com/questions/3959924/whats-the-difference-between-git-clone-mirror-and-git-clone-bare) - just to know what to backup
+- [Mirror Repo to new one](https://medium.com/cloud-native-the-gathering/how-to-mirror-copy-an-entire-existing-git-repository-into-a-new-one-3bb8faefad9e) - transfer info for students
+- [GitHub Transfer](https://docs.github.com/en/github/administering-a-repository/managing-repository-settings/transferring-a-repository) - transfer info for admin/teacher
+
+### Image Storage
+
+The following links give just some ideas how to store images on github (and to prevent big chunks of binary data by deleting the extra branch in case of new images).
+
+- [Images in branch](https://medium.com/@minamimunakata/how-to-store-images-for-use-in-readme-md-on-github-9fb54256e951)
+- [Images in branch - gist](https://gist.github.com/joncardasis/e6494afd538a400722545163eb2e1fa5)
+- [Delete branch](https://www.makeuseof.com/how-to-delete-branch-git/)
+
+## Backup Batch Script
+
+### Preparation
+
+1. Create a "backup token" - visit <https://github.com/settings/tokens/new>
+   - check at least  `repo`, `admin:org -> read:org`, and `user` checkboxes and press `Generate Token`
+   - copy the generated token for later (and store it for example within a password-manager)
+2. DONE!
+
+### Own Script with GraphQL - Just Because We Can
+
+1. Read and learn: <https://www.robinwieruch.de/getting-started-github-graphql-api> (part 1 and 2)
+   - look especially at the 2nd part (pagination)
+2. Tryout: <https://docs.github.com/en/graphql/overview/explorer>
+3. use the two `graphql`-files within this folder.
+
+### Alternative - PowerShellForGitHub Module
+
+Use <https://github.com/microsoft/PowerShellForGitHub> as basis - makes life much easier (but uses REST API). For Setup follow the Installation and Configuration steps from the readme there. Here the short version:
+
+1. `Install-Module -Name PowerShellForGitHub -Scope CurrentUser`
+2. Create a new API token by going to <https://github.com/settings/tokens/new> (provide a description and check any appropriate scopes).
+3. Call `Set-GitHubAuthentication`, enter anything as the username (the username is ignored but required by the dialog that pops up), and paste in the API token as the password. That will be securely cached to disk and will persist across all future PowerShell sessions. If you ever wish to clear it in the future, just call `Clear-GitHubAuthentication`).
+4. DONE!
+
+Some usage examples:
+
+```powershell
+# HELP
+Get-Help Get-GitHubRepository -Examples
+
+# ORG
+$sew2repos = Get-GitHubRepository -OrganizationName litec-SEW2
+$sew2repos.count
+
+# USER
+# ALL repos where current authenticated user has access to (can be a long list!)
+$repos = Get-GitHubRepository
+# owned repos
+$ownedRepos = Get-GitHubRepository -Affiliation owner
+```
